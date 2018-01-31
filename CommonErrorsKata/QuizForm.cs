60615b0ad3aaff5968521287021873ec6e92a98d
Starla Oden <starlaoh@gmail.com>
2018-01-31 11:18:24 -0800

    Test now returns false for wrong answers

--- CommonErrorsKata/QuizForm.cs
+++ CommonErrorsKata/QuizForm.cs
@@ -14,17 +14,21 @@ namespace CommonErrorsKata
         private readonly string[] _files;
         private readonly string[] _possibleAnswers;
         private readonly SynchronizationContext _synchronizationContext;
+        private readonly int _maxAnswers = 15;
         private int _time = 100;
-        private string _currentBaseName;
+        private string _visibleImageName;
 
         public CommonErrorsForm()
         {
             InitializeComponent();
             _synchronizationContext = SynchronizationContext.Current;
-            _files = Directory.GetFiles(Environment.CurrentDirectory +  @"..\..\..\ErrorPics");
-            _possibleAnswers = new[] { "Missing File", "null instance", "divide by zero" };
+            _files = Directory.GetFiles(Environment.CurrentDirectory + @"..\..\..\ErrorPics");
+
+            //_possibleAnswers = _files.Select(f) => f.Split(@"\".ToCharArray()).Last().Replace(".png", " ")).ToArray();
+
+            _possibleAnswers = new[] { "Missing File", "Null Instance", "Divide By Zero" };
             lstAnswers.DataSource = _possibleAnswers;
-            _answerQueue = new AnswerQueue<TrueFalseAnswer>(15);
+            _answerQueue = new AnswerQueue<TrueFalseAnswer>(_maxAnswers);
             Next();
             lstAnswers.Click += LstAnswers_Click;
             StartTimer();
@@ -44,22 +48,26 @@ namespace CommonErrorsKata
 
         private void LstAnswers_Click(object sender, EventArgs e)
         {
+
             _time = 100;
-            var tokens = _currentBaseName.Split(' ');
+            var tokens = _visibleImageName.Split(' ');
             var selected = _possibleAnswers[lstAnswers.SelectedIndex];
             //TODO:  Figure out what is a valid answer.
 
-            if (selected != null && selected == (string) lstAnswers.SelectedItem)
+            if (selected != null && selected == _visibleImageName)
             {
                 _answerQueue.Enqueue(new TrueFalseAnswer(true));
             }
-
+            else
+            {
+                _answerQueue.Enqueue(new TrueFalseAnswer(false));
+            }
             Next();
         }
 
         private void Next()
         {
-            if (_answerQueue.Count == 15 && _answerQueue.Grade >= 98)
+            if (_answerQueue.Count == _maxAnswers && _answerQueue.Grade >= 98)
             {
                 MessageBox.Show("Congratulations you've defeated me!");
                 Application.Exit();
@@ -67,13 +75,14 @@ namespace CommonErrorsKata
             }
             label1.Text = _answerQueue.Grade + "%";
             var file = _files.GetRandom();
-            _currentBaseName= Path.GetFileName(file);
+            _visibleImageName = Path.GetFileName(file) != null ? Path.GetFileName(file)?.Replace(".png", "") : null;
             pbImage.ImageLocation = file;
         }
 
         public void UpdateProgress(int value)
         {
-            _synchronizationContext.Post(x => {
+            _synchronizationContext.Post(x =>
+            {
                 progress.Value = value;
             }, value);
         }
